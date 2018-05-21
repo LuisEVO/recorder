54f5b6fc19ef2633ca07b8e5bc098c61e452365d
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2018-05-21 11:20:05 -0500

    recorder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { Media } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Media,
+    File,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
